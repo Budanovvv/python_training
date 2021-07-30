f3d36c2e2e45fe37a10e0d0aca1546924884de69
Valentyn Budanov  <valentyn.budanov@uapay.ua>
2021-07-30 10:19:30 +0300

    Задание №4: Перевести тесты с unittest на py.test и реализовать фикстуры

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,158 +1,41 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from contact import Contact
-import unittest
+from application import Application
+import pytest
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def test_add_group(self):
-        self.login(user="admin", password="secret")
-        self.fill_contact(Contact(name_frst="Petr",
-                                  name_mdl="Petrovych",
-                                  name_lst="Petrov",
-                                  name_nick="PetrCompNick",
-                                  comp_title="PetrComp_1111",
-                                  comp_name="PetrComp",
-                                  comp_addr="Ulica 2",
-                                  home_ph="111-111-111",
-                                  mobile_ph="222-222-222",
-                                  work_ph="333-333-333",
-                                  fax_ph="444-444-444",
-                                  email_1="111111@example.com",
-                                  email_2="222222@example.com",
-                                  email_3="333333@example.com",
-                                  home_page="example.com",
-                                  b_day="3",
-                                  b_month="March",
-                                  b_year="2000",
-                                  a_day="4",
-                                  a_month="February",
-                                  a_year="2010",
-                                  secondary_address="Secondary Address unknown",
-                                  secondary_phone="Secondary phone ?",
-                                  secondary_notes="Secondary notes"))
-
-        self.logout()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("home page").click()
-        wd.find_element_by_link_text("Logout").click()
-
-    def fill_contact(self, contact):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.name_frst)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.name_mdl)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.name_lst)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.name_nick)
-        # title_company_address
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.comp_title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.comp_name)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.comp_addr)
-        # phones
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_ph)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_ph)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work_ph)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax_ph)
-        # emails_homepage
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email_1)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email_2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email_3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.home_page)
-        # anniversary_group
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.b_day)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.b_month)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.b_year)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.a_day)
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.a_month)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.a_year)
-        wd.find_element_by_name("new_group").click()
-        # secondary
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.secondary_address)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.secondary_phone)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.secondary_notes)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
+def test_add_group(app):
+    app.login(user="admin", password="secret")
+    app.fill_contact(Contact(name_frst="Petr",
+                              name_mdl="Petrovych",
+                              name_lst="Petrov",
+                              name_nick="PetrCompNick",
+                              comp_title="PetrComp_1111",
+                              comp_name="PetrComp",
+                              comp_addr="Ulica 2",
+                              home_ph="111-111-111",
+                              mobile_ph="222-222-222",
+                              work_ph="333-333-333",
+                              fax_ph="444-444-444",
+                              email_1="111111@example.com",
+                              email_2="222222@example.com",
+                              email_3="333333@example.com",
+                              home_page="example.com",
+                              b_day="3",
+                              b_month="March",
+                              b_year="2000",
+                              a_day="4",
+                              a_month="February",
+                              a_year="2010",
+                              secondary_address="Secondary Address unknown",
+                              secondary_phone="Secondary phone ?",
+                              secondary_notes="Secondary notes"))
 
-    def login(self, user, password):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/edit.php")
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(user)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("LoginForm").submit()
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
+    app.logout()
