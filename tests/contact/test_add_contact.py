b38b676a960aeedaac918e135ea0ae0d5265b3c1
Valentyn Budanov  <valentyn.budanov@uapay.ua>
2021-08-30 18:59:40 +0300

    Study GIT 111

--- tests/contact/test_add_contact.py
+++ tests/contact/test_add_contact.py
@@ -102,3 +102,5 @@ def test_add_contact(app, contact):
 #     assert len(old_contacts) + 1 == app.contact.count()
 #     old_contacts.append(contact)
 #     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
