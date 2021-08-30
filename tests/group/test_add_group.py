a3d95992a0e310c53c1d0023d07b5641227222ab
Valentyn Budanov  <valentyn.budanov@uapay.ua>
2021-08-30 14:25:24 +0300

    Параметризовал тесты для создания групп- fix

--- tests/group/test_add_group.py
+++ tests/group/test_add_group.py
@@ -20,8 +20,8 @@ def random_string(prefix, maxlen):
 
 testdata = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name - ", 10),
-          header=random_string("header - ", 10),
-          footer=random_string("footer - ", 10))
+          header=random_string("header - ", 20),
+          footer=random_string("footer - ", 20))
     for i in range(5)
 ]
 
@@ -34,13 +34,3 @@ def test_add_group(app, group):
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == app.group.count()
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- tests/group/test_update_groups.py
+++ tests/group/test_update_groups.py
@@ -1,14 +1,38 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 from random import randrange
+import pytest
+import random
+import string
 
 
-def test_update_rnd_group_name(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+# testdata = [
+#     Group(name=name, header=header, footer=header)
+#     for name in ["", random_string("name ", 10)]
+#     for header in ["", random_string("header ", 20)]
+#     for footer in ["", random_string("footer ", 20)]
+# ]
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name - ", 10),
+          header=random_string("header - ", 10),
+          footer=random_string("footer - ", 10))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_update_rnd_group_name(app, group):
     if app.group.count() == 0:
         app.group.create(Group(name="Create group for Updated"))
     old_groups = app.group.get_group_list()
     index = randrange(len(old_groups))
-    group = Group(name="Updated group2")
     group.id = old_groups[index].id
     app.group.update_group_by_index(index, group)
     new_groups = app.group.get_group_list()
