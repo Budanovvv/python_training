9f32cd2b6b61741897978a9c9c8cc0ec2f5cd3dd
Budanovvv <budanov.vv@gmail.com>
2021-08-31 10:58:23 +0300

    Задание №16: Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -7,10 +7,12 @@ fixture = None
 
 
 @pytest.fixture
-def app():
+def app(request):
     global fixture
     if fixture is None:
-        fixture = Application()
+        browser = request.config.getoption("--browser")
+        base_url = request.config.getoption("--baseUrl")
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
             fixture = Application()
@@ -25,3 +27,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
--- fixture/application.py
+++ fixture/application.py
@@ -7,16 +7,22 @@ from fixture.random import RandomHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.contact = ContactHelper(self)
         self.group = GroupHelper(self)
         self.random = RandomHelper(self)
+        self.base_url = base_url
 
     def main_url(self):
-        self.wd.get("http://localhost/addressbook/index.php")
+        self.wd.get(self.base_url)
 
     def is_valid(self):
         try:
