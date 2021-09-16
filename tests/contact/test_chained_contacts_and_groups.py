8b8804fb2e151f5e88aa18f7fde0e2fb0b495280
valentinebudanov <budanov.vv@gmail.com>
2021-09-16 09:48:14 +0300

    Задание №22: В тест по добавлению контакта -- добавил правило поиска любого контакта не из группы В тест по удалению контакта -- добавил проверку что если контакта нет в группе то добавить

--- tests/contact/test_chained_contacts_and_groups.py
+++ tests/contact/test_chained_contacts_and_groups.py
@@ -8,10 +8,10 @@ def test_check_add_contact_to_the_group(app, orm, db):
         app.contact.create(Contact(firstname="Petr"))
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="First"))
-    contact = random.choice(orm.get_contact_list())
-    contact_id = contact.id
     group = random.choice(orm.get_group_list())
     group_id = group.id
+    contact = random.choice(orm.get_contacts_not_in_group(group))
+    contact_id = contact.id
     app.contact.add_contact_to_the_group(contact_id, group_id)
     assert db.get_contact_by_id(contact_id) in orm.get_contacts_in_group(Group(id=group_id))
 
@@ -25,6 +25,7 @@ def test_check_delete_contact_from_the_group(app, orm, db):
     contact_id = contact.id
     group = random.choice(orm.get_group_list())
     group_id = group.id
-    app.contact.add_contact_to_the_group(contact_id, group_id)
+    if not orm.get_contacts_in_group(group):
+        app.contact.add_contact_to_the_group(contact_id, group_id)
     app.contact.del_contact_from_the_group(contact_id, group_id)
     assert db.get_contact_by_id(contact_id) not in orm.get_contacts_in_group(Group(id=group_id))
